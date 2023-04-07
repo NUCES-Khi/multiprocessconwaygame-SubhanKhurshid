b23b6115afe80e4f4e02baf997f2d23a4a372d89
Muhammad Subhan Khurshid <105592966+SubhanKhurshid@users.noreply.github.com>
2023-04-07 05:46:21 +0500

    Update main.c

--- main.c
+++ main.c
@@ -1,32 +1,94 @@
-/**
- * @defgroup   MAIN main
- *
- * @brief      This file implements main. Please Comment you code
- *
- * @author     Enter Your name and Student ID here.
- * @date       2023
- */
-
-#include <sys/mman.h>
-#include <fcntl.h>
-#include <semaphore.h>
-#include <sys/stat.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <time.h>
 #include <unistd.h>
+#include <sys/wait.h>
 
+#define rows 10
+#define columns 10
 
-/**
- * @brief      { function_description }
- *
- * @param[in]  argc  The count of arguments
- * @param      argv  The arguments array
- *
- * @return     { description_of_the_return_value }
- */
-int main(int argc, char const *argv[])
-{
-    // It is left up to you how you use shared memory and multiple processes 
-    // to solve the conway problem
+void InitializingMatrix(int matrix[rows][columns]) {
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<columns; j++) {
+            matrix[i][j] = rand() % 2;
+        }
+    }
+}
+
+void print_matrix(int matrix[rows][columns]) {
+    printf("\033[2J"); 
+    printf("\033[%d;%dH", 0, 0); 
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<columns; j++) {
+            if(matrix[i][j] == 1) {
+                printf("* ");
+            } else {
+                printf(". ");
+            }
+        }
+        printf("\n");
+    }
+}
+
+int count_neighbors(int matrix[rows][columns], int row, int col) {
+    int count = 0;
+    for(int i=row-1; i<=row+1; i++) {
+        for(int j=col-1; j<=col+1; j++) {
+            if(i < 0 || i >= rows || j < 0 || j >= columns) {
+                continue;
+            } else if(i == row && j == col) {
+                continue;
+            } else if(matrix[i][j] == 1) {
+                count++;
+            }
+        }
+    }
+    return count;
+}
+
+void UpdatingTheMatrix(int matrix[rows][columns], int new_matrix[rows][columns]) {
+    for(int i=0; i<rows; i++) {
+        for(int j=0; j<columns; j++) {
+            int neighbors = count_neighbors(matrix, i, j);
+            if(matrix[i][j] == 1 && (neighbors < 2 || neighbors > 3)) {
+                new_matrix[i][j] = 0;
+            } else if(matrix[i][j] == 0 && neighbors == 3) {
+                new_matrix[i][j] = 1;
+            } else {
+                new_matrix[i][j] = matrix[i][j];
+            }
+        }
+    }
+}
+
+int main() {
+    int matrix[rows][columns];
+    int new_matrix[rows][columns];
+
+    srand(time(NULL));
+    InitializingMatrix(matrix);
+    InitializingMatrix(new_matrix);
+
+    while(1) {
+        UpdatingTheMatrix(matrix, new_matrix);
+        print_matrix(new_matrix);
+        int pid = fork();
+        if(pid == 0){
+            for(int i=0; i<rows; i++) {
+                for(int j=0; j<columns; j++) {
+                    matrix[i][j] = new_matrix[i][j];
+                }
+            }
+            sleep(1);
+            exit(0);
+        }else{
+            for(int i=0; i<rows; i++) {
+                for(int j=0; j<columns; j++) {
+                    new_matrix[i][j] = matrix[i][j];
+                }
+            }
+            wait(NULL);
+        }
+    }
     return 0;
 }
